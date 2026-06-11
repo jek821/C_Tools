59c1ef7c13c729696dd5f1ee6e11a1e2b43458d5
Jemanuel <jacobkriss2001@gmail.com>
2026-06-10 22:53:18 -0400

    polished jek_cat, abstracted helpers and fixed error handling, and partial writes

--- jek_cat/jekcat.c
+++ jek_cat/jekcat.c
@@ -1,34 +1,62 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 
+int write_all_bytes(int output_fd, const char *buffer, ssize_t bytes_to_write) {
+    ssize_t total_bytes_written = 0;
 
-char buffer[10];
-ssize_t bytes_read;
+    while (total_bytes_written < bytes_to_write) {
 
-int main(int argc, char* argv[]){
-    if (argc > 1){
-        int fd = open(argv[1], O_RDONLY);
-        if (fd == -1){
-            perror("open");
+        ssize_t bytes_written =
+            write(output_fd, buffer + total_bytes_written, bytes_to_write - total_bytes_written);
+        if (bytes_written == -1) {
+            perror("write");
+            return 1;
+        }
+        total_bytes_written += bytes_written;
+    }
+    return 0;
+}
+
+int copy_fd_to_stdout(int input_fd) {
+    char buffer[4096];
+    ssize_t bytes_read;
+
+    while (1) {
+        bytes_read = read(input_fd, buffer, sizeof(buffer));
+
+        if (bytes_read == -1) {
+            perror("read");
+            close(input_fd);
             return 1;
         }
 
-        while (1){
-            bytes_read = read(fd, buffer, 10);
-            if (bytes_read == -1){
-                perror("read");
-                close(fd);
-                return 1;
-            }
-            if (bytes_read == 0){
-                close(fd);
-                return 0;
-            }
+        if (bytes_read == 0) {
+            close(input_fd);
+            return 0;
+        }
 
-            if (bytes_read > 0){
-                write(STDOUT_FILENO, buffer, bytes_read);
-            }
+        int write_status = write_all_bytes(STDOUT_FILENO, buffer, bytes_read);
+
+        if (write_status != 0) {
+            close(input_fd);
+            return 1;
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        fprintf(stderr, "usage: mycat <file>\n");
+        return 1;
+    }
+
+    int input_fd = open(argv[1], O_RDONLY);
+
+    if (input_fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    return copy_fd_to_stdout(input_fd);
 }
